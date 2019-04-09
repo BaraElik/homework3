e1f347e9de824fd533854178014cd6355f8550bc
BaraElik <BaraElik>
2019-04-09 18:52:52 +0200

    version 1.2

--- app/src/main/java/com/example/homework3/Movies.java
+++ app/src/main/java/com/example/homework3/Movies.java
@@ -1,6 +1,9 @@
 package com.example.homework3;
 
-public class Movies {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movies implements Parcelable {
 
     public int image;
     public String title;
@@ -12,5 +15,38 @@ public class Movies {
         this.title = title;
         this.rating = rating;
         this.info = info;
+    }
+
+    protected Movies(Parcel in) {
+        image = in.readInt();
+        title = in.readString();
+        rating = in.readString();
+        info = in.readString();
+    }
+
+    public static final Creator<Movies> CREATOR = new Creator<Movies>() {
+        @Override
+        public Movies createFromParcel(Parcel in) {
+            return new Movies(in);
+        }
+
+        @Override
+        public Movies[] newArray(int size) {
+            return new Movies[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+
+        parcel.writeInt(image);
+        parcel.writeString(title);
+        parcel.writeString(rating);
+        parcel.writeString(info);
     }
 }
